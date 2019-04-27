b5a235ddc1a5a0c4b7259cbb67cdf3caebbef82a
tpluscode <tpluscode@users.noreply.github.com>
2019-04-27 11:31:59 +0200

    test endpoint to debug settings

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,8 @@ WORKDIR /app
 
 COPY --from=build-env /output .
 
+ENV wikibus:sources:sql=$wikibus:sources:sql
+
 RUN echo "ASPNETCORE_URLS=http://0.0.0.0:\$PORT\nDOTNET_RUNNING_IN_CONTAINER=true" > /app/setup_heroku_env.sh && chmod +x /app/setup_heroku_env.sh
 
 CMD /bin/bash -c "source /app/setup_heroku_env.sh && dotnet app.dll"
---
+++ src/app/ConfigModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Nancy;
+
+namespace Brochures.Wikibus.Org
+{
+    public class ConfigModule : NancyModule
+    {
+        public ConfigModule(IConfiguration configuration)
+        {
+            this.Get("/config", _ => this.Response.AsText(string.Join(
+                    Environment.NewLine,
+                    configuration.AsEnumerable().Select(p => $"{p.Key} => {p.Value}"))));
+        }
+    }
+}
