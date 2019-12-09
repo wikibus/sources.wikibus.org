cd5f7cc04712c41c7661b68ce1f82aa3d905c01c
tpluscode <tpluscode@users.noreply.github.com>
2019-12-09 19:26:21 +0100

    ci: typo in command

--- .build/certs.sh
+++ .build/certs.sh
@@ -2,4 +2,4 @@
 
 cp -r ~/.lando/certs/lndo.site.pem /usr/local/share/ca-certificates/lndo.site.pem
 cp -r ~/.lando/certs/lndo.site.crt /usr/local/share/ca-certificates/lndo.site.crt
-update-ca-certificate
+update-ca-certificates
