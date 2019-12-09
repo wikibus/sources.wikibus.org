10efc1f32133869d373421e85f1b1261d56e5131
tpluscode <tpluscode@users.noreply.github.com>
2019-12-09 19:20:15 +0100

    ci: try cert paths as documented at lando

--- .build/certs.sh
+++ .build/certs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp -r /lando/certs/lndo.site.pem /usr/local/share/ca-certificates/lndo.site.pem
-cp -r /lando/certs/lndo.site.crt /usr/local/share/ca-certificates/lndo.site.crt
+cp -r ~/.lando/certs/lndo.site.pem /usr/local/share/ca-certificates/lndo.site.pem
+cp -r ~/.lando/certs/lndo.site.crt /usr/local/share/ca-certificates/lndo.site.crt
 update-ca-certificate
