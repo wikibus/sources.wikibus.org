a5229bc7944e02797da4e8560a8a7e3bd4a17f48
tpluscode <tpluscode@users.noreply.github.com>
2019-12-12 08:15:51 +0100

    build: fix the certificate problem with docker-compose

--- .build/certs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -r ~/.lando/certs/lndo.site.pem /usr/local/share/ca-certificates/lndo.site.pem
-cp -r ~/.lando/certs/lndo.site.crt /usr/local/share/ca-certificates/lndo.site.crt
-update-ca-certificates
