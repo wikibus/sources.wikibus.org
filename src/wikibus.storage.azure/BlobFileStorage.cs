63020b24bbf7ab92d4beb221caf3afcffbf959f9
tpluscode <tpluscode@users.noreply.github.com>
2020-03-01 21:46:39 +0100

    chore: add some logs to azure storage code

--- src/wikibus.storage.azure/BlobFileStorage.cs
+++ src/wikibus.storage.azure/BlobFileStorage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Anotar.Serilog;
 using Azure.Storage.Blobs;
 using Microsoft.Extensions.Azure;
 using Microsoft.WindowsAzure.Storage;
@@ -10,21 +11,24 @@ namespace wikibus.storage.azure
 {
     public class BlobFileStorage : IFileStorage
     {
-        private readonly IAzureSettings settings;
+        private readonly CloudStorageAccount account;
 
         public BlobFileStorage(IAzureSettings settings)
         {
-            this.settings = settings;
+            LogTo.Debug("Connecting to Azure Storage");
+            this.account = CloudStorageAccount.Parse(settings.ConnectionString);
         }
 
         public async Task<Uri> UploadFile(string name, string container, string contentType, Stream contents)
         {
-            var account = CloudStorageAccount.Parse(this.settings.ConnectionString);
+            LogTo.Debug("Creating blob client");
             var client = account.CreateCloudBlobClient();
 
+            LogTo.Debug("Getting blob container {0}", container);
             var blobRef = client.GetContainerReference(container);
             if (await blobRef.CreateIfNotExistsAsync()) {
 
+                LogTo.Information("Creating blob container {0}", container);
                 await blobRef.SetPermissionsAsync(new BlobContainerPermissions
                 {
                     PublicAccess = BlobContainerPublicAccessType.Blob,
@@ -35,6 +39,7 @@ namespace wikibus.storage.azure
             var blob = blobRef.GetBlockBlobReference(name);
             blob.Properties.ContentType = contentType;
 
+            LogTo.Information("Uploading file {)}", name);
             await blob.UploadFromStreamAsync(contents);
 
             return blob.Uri;
