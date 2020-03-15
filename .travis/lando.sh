8988b6cfebde5e7c1323b24bd3fd36e08adcd1de
tpluscode <tpluscode@users.noreply.github.com>
2020-03-15 21:18:44 +0100

    ci: travis deployment

--- .travis/lando.sh
+++ .travis/lando.sh
@@ -5,3 +5,5 @@ sudo apt-get -y install cgroup-bin curl
 curl -fsSL -o /tmp/lando-latest.deb https://github.com/lando/lando/releases/download/v3.0.0-rc.23/lando-v3.0.0-rc.23.deb
 sudo dpkg -i /tmp/lando-latest.deb
 lando version
+lando start
+npx wait-on --timeout 30000 http://wikibus-sources.lndo.site
