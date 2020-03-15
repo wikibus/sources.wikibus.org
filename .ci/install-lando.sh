acc6cfaa68c102fba5609eba1b2cc1e5711f310b
tpluscode <tpluscode@users.noreply.github.com>
2020-03-15 23:01:05 +0100

    ci: move to github actions

--- .ci/install-lando.sh
+++ .ci/install-lando.sh
@@ -7,5 +7,3 @@ sh get-docker.sh
 curl -fsSL -o /tmp/lando-latest.deb https://github.com/lando/lando/releases/download/v3.0.0-rc.23/lando-v3.0.0-rc.23.deb
 sudo dpkg -i /tmp/lando-latest.deb
 lando version
-lando start
-npx wait-on --timeout 30000 http://wikibus-sources.lndo.site
