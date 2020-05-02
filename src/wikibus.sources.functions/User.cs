9fc935adfbed367c25801d0dd62bdd796fb691d3
tpluscode <tpluscode@users.noreply.github.com>
2020-05-02 11:29:39 +0200

    feat: endpoint to get all contributors

--- src/wikibus.sources.functions/User.cs
+++ src/wikibus.sources.functions/User.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Anotar.Serilog;
+using Argolis.Hydra.Resources;
 using Auth0.Core.Exceptions;
+using Auth0.ManagementApi.Models;
+using Auth0.ManagementApi.Paging;
 using JsonLD.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Wikibus.Sources.EF;
 using Wikibus.Sources.Functions.Model;
 
 namespace Wikibus.Sources.Functions
@@ -16,12 +22,17 @@ namespace Wikibus.Sources.Functions
     {
         private readonly ManagementClientFactory clientFactory;
         private readonly IConfiguration configuration;
+        private readonly EntitySerializer entitySerializer = new EntitySerializer();
+        private readonly ISourceContext context;
 
-        public User(ManagementClientFactory clientFactory, IConfiguration configuration)
+        public User(ManagementClientFactory clientFactory, IConfiguration configuration, ISourceContext context)
         {
             this.clientFactory = clientFactory;
             this.configuration = configuration;
+            this.context = context;
         }
+
+        private string ContributorRoleId => this.configuration["authentication:Auth0:Management:Roles:Contributor"];
 
         [FunctionName("User")]
         public async Task<IActionResult> GetUser(
@@ -53,7 +64,43 @@ namespace Wikibus.Sources.Functions
 
             return new ContentResult
             {
-                Content = new EntitySerializer().Serialize(person).ToString(),
+                Content = this.entitySerializer.Serialize(person).ToString(),
+                ContentType = "application/ld+json"
+            };
+        }
+
+        [FunctionName("Users")]
+        public async Task<IActionResult> GetAllUsers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "contributors")]
+            HttpRequest request)
+        {
+            var auth0Client = await this.clientFactory.Create();
+            var userIds = await this.context.Brochures.Select(brochure => brochure.User).Distinct().ToListAsync();
+            var userIdQuery = string.Join(" or ", userIds.Select(id => $"user_id:\"{id}\""));
+
+            var users = await auth0Client.Users.GetAllAsync(
+                new GetUsersRequest
+                {
+                    Query = userIdQuery
+                },
+                new PaginationInfo(includeTotals: true));
+
+            var members = users.Select(user => new Person
+            {
+                Id = new Uri($"{this.configuration["wikibus:usersUrl"]}user/{user.UserId}"),
+                Name = user.FullName,
+            }).ToArray();
+
+            var collection = new Collection<Person>
+            {
+                Id = new Uri($"{this.configuration["wikibus:usersUrl"]}user/contributors"),
+                Members = members,
+                TotalItems = users.Paging.Total,
+            };
+
+            return new ContentResult
+            {
+                Content = this.entitySerializer.Serialize(collection).ToString(),
                 ContentType = "application/ld+json"
             };
         }
