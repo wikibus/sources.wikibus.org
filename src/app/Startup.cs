8b1914f1512b5f9b2f777a615a7f36f7ee8cf665
tpluscode <tpluscode@users.noreply.github.com>
2022-12-14 21:27:19 +0100

    fix: AllowSynchronousIO

--- src/app/Startup.cs
+++ src/app/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +32,11 @@ namespace Brochures.Wikibus.Org
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
             string domain = $"https://{this.Configuration["authentication:Auth0:Domain"]}/";
             services.AddAuthentication(options =>
             {
